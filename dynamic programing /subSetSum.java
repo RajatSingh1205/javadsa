41bda02808eb4e9a8b5886dd71dd30e3981f2841
Rajat Singh <135345654+RajatSingh1205@users.noreply.github.com>
2026-02-19 19:46:58 +0530

    Correct dp array initialization in subSetSum.java
    Fix initialization of dp array for subset sum problem.

--- dynamic programing /subSetSum.java
+++ dynamic programing /subSetSum.java
@@ -24,11 +24,11 @@ public class DP {
         boolean[][] dp = new boolean[n+1][sum + 1];
 
         for (int i = 0; i <= n; i++) {
-            dp[i][0] = false;
+            dp[i][0] = true;
         }
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= n; j++) {
+            for (int j = 1; j <= n; j++) {
                 if (arr[i - 1] <= j) {
                     dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i - 1][j];
                 } else {
